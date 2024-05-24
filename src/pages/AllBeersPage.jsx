2aa913cf312c5a03780bb81d6177735393378e7a
Franciely Beckert <francielybeckert@gmail.com>
2024-05-24 20:10:56 +0200

    Do up to 4

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,56 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function AllBeersPage() {
+  const [beers, setBeers] = useState([]);
+
+  const [fetching, setfetching] = useState(true);
+
+  useEffect(() => {
+    console.log("useEffect - Initial render (Mounting)");
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => {
+        setBeers(response.data);
+        console.log("response.data: ", response.data);
+        setfetching(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching biers data:", error);
+        setfetching(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {fetching ? (
+        <p>Loading</p>
+      ) : (
+        beers.map((beer) => {
+          return (
+            <div key={beer._id} className="allbeerspage">
+              <img
+                src={beer.image_url}
+                alt={beer.name}
+                className="allbeerspage-image"
+              />
+              <div className="allbeerspage-text">
+                <Link to={`/beers/${beer._id}`}>
+                  <h1 className="allbeerspage-name">{beer.name}</h1>
+                </Link>
+
+                <h2 className="allbeerspage-tagline">{beer.tagline}</h2>
+                <p className="allbeerspage-contributed-by">
+                  Created by: {beer.contributed_by}
+                </p>
+              </div>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+}
 
 export default AllBeersPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,42 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+import allBeersImage from "../assets/beers.png";
+import randomBeerImage from "../assets/random-beer.png";
+import newBeerImage from "../assets/new-beer.png";
+
+function HomePage() {
+    return (
+        <div className="homepage">
+        <nav>
+            <ul className="homepage-ul">
+                <li className="homepage-list">
+                <img className="homepage-image" src={allBeersImage}/>
+                <Link className="homepage-link" to="/beers">All Beers</Link>
+                <p className="homepage-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </li>
+            </ul>
+        </nav>
+        <nav>
+            <ul className="homepage-ul">
+                <li className="homepage-list">
+                <img  className="homepage-image" src={randomBeerImage}/>
+                <Link className="homepage-link" to="/random-beer">Random Beer</Link>
+                <p className="homepage-text" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </li>
+            </ul>
+        </nav>
+        <nav>
+            <ul className="homepage-ul">
+                <li className="homepage-list">
+                <img className="homepage-image" src={newBeerImage}/>
+                <Link className="homepage-link" to="/new-beer">New Beer</Link>
+                <p className="homepage-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </li>
+            </ul>
+        </nav>
+       
+        
+        </div>
+    )
+}
 
 export default HomePage;
